b2833d651aa1e0bf46daa9dfd764688770e3b1d9
JoaoVitorN-dev <joaovitornepomuceno.dev@gmail.com>
2025-05-06 20:50:52 -0400

    feat: :sparkles: add api to shorts url's

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,28 @@
 let btnUrl = document.querySelector('#btnUrl');
 
-btnUrl.addEventListener('click', () =>{
-    
+btnUrl.addEventListener('click', () => {
+    const url = document.querySelector('#url').value;
+    const token = '5NSl45RqZnCyM99Cy15GXWP1vvSHR6yeThchSZgNgmTUVRzScIUIRbeKdm1e';
+
+    fetch('https://api.tinyurl.com/create', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+
+        },
+        body: JSON.stringify({ url: url })
+    }).then(response => response.json())
+        .then(data => {
+            const showUrl = document.querySelector('.boxUrl');
+
+            showUrl.innerHTML += `
+            <div class="shortUrl">
+                <p>${data.data.url}</p>
+                <p>${data.data.tiny_url}</p>
+
+            </div>
+        `
+        })
+
 })
